603020257c97b3443d8fc307dd4dbb896c0d408c
chapi <chapi@localhost.localdomain>
2020-03-20 21:51:17 -0400

    Final release

--- bot.js
+++ bot.js
@@ -1,101 +1,125 @@
+const process = require('dotenv').config().parsed
 
+// console.log(process)
 
-const TOKEN = process.env.TELEGRAM_TOKEN || 'YOUR_TELEGRAM_BOT_TOKEN';
+const TOKEN = process.TELEGRAM_TOKEN ;
 const TelegramBot = require('node-telegram-bot-api');
 const request = require('request');
 const axios = require('axios')
+var fs = require('fs');
+var countries = require('./country');
+var rev_country = require('./rev country')
+// console.log(rev_country.ethiopia);
+
+// console.log(countries.ET)
 const options = {
   polling: true
 };
-const bot = new TelegramBot("913957091:AAEa5aAq4WYxuzYyEQ1gyh2CKDAuczZX4BU", options);
+const bot = new TelegramBot(TOKEN, options);
 
+// to match the word start or /start in case insensetive
+bot.onText(/(\/|)start/ig, function start(msg){
+  bot.sendMessage(msg.chat.id, `* Send me the word <b><i>World</i></b> to see global data
+* send me country name or country code to see for specific country like <b>Ethiopia</b> or <b>ET</b>,<b>Italy</b> or <b>IT</b>, `,
+{ parse_mode: "HTML" })
+return;
+})
 
-// Matches /photo
-bot.onText(/world/ig, async function onPhotoText(msg) {
-  
+// Matches world text 
+bot.onText(/world/ig, async function world(msg) {
+  bot.sendMessage(msg.chat.id, "your request is loading.please wait ...");
   try{
-    var worldCovidData = await axios.get('https://covid19.mathdro.id/api');
-    console.log(worldCovidData.data.confirmed.value)
-    bot.sendMessage(msg.chat.id, "World Covid Data is loading\n please wait ...");
-    bot.sendMessage(msg.chat.id, `Coronavirus COVID-19 Global Cases by the Center for Systems Science and Engineering (CSSE)\n 
-The confirmed Case is ${worldCovidData.data.confirmed.value}\n
-Recovered Patient is ${worldCovidData.data.recovered.value}\n and 
-${worldCovidData.data.deaths.value} deaths.`) 
+    var worldCovidData = await axios.get('https://thevirustracker.com/free-api?global=stats');
+    var worldCovidData = await worldCovidData.data.results[0]
+    // console.log( worldCovidData.total_cases)
+    // console.log("ended")
+    bot.sendMessage(msg.chat.id, `CoronaVirus covid-19: 
+Confirmed Case: <b>${worldCovidData.total_cases}</b> 
+Recovered:  <b>${worldCovidData.total_recovered}</b> 
+Death:  <b>${worldCovidData.total_deaths}</b>
+Total new cases today:  <b>${worldCovidData.total_new_cases_today}</b>
+Total new deaths today: <b>${worldCovidData.total_new_deaths_today}</b>
+Total active cases :    <b>${worldCovidData.total_active_cases}</b>
+Total serious cases :   <b>${worldCovidData.total_serious_cases}</b>
 
+<b>You can send me country name or county code to get info about that country</b>
+Example: <code>Ethiopia</code> or <code>ET</code>` ,  { parse_mode: "HTML" })
 
   }catch(err){
     console.log(err.message)
-    bot.sendMessage(msg.chat.id, `There is Network Error from our Server please try later!\n
-    or Try Contact @chapimenge3`)
+     bot.sendMessage(msg.chat.id, `There is Network Error from our Server please try later!\n
+    or Try Contacting the admin: @chapimenge`) 
   }
-  // var data = JSON.parse()
 });
 
+function toTitleCase(str) {
+  // console.log(str);
+  
+  return str.replace(
+      /\w\S*/g,
+      function(txt) {
+          return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+      }
+  );
+}
 
-// Matches /any word other than world
-bot.onText(/^((?!world).)*$/gim, function onAudioText(msg) {
-  // From HTTP request
-  const url = 'https://upload.wikimedia.org/wikipedia/commons/c/c8/Example.ogg';
-  const audio = request(url);
-  bot.sendMessage(msg.chat.id, "The  is...");
-});
-
-
-// Matches /love
-bot.onText(/\/love/, function onLoveText(msg) {
-  const opts = {
-    reply_to_message_id: msg.message_id,
-    reply_markup: JSON.stringify({
-      keyboard: [
-        ['Yes, you are the bot of my life ❤'],
-        ['No, sorry there is another one...']
-      ]
-    })
-  };
-  bot.sendMessage(msg.chat.id, 'Do you love me?', opts);
-});
-
-
-// Matches /echo [whatever]
-bot.onText(/\/echo (.+)/, function onEchoText(msg, match) {
-  const resp = match[1];
-  bot.sendMessage(msg.chat.id, resp);
-});
-
-
-// Matches /editable
-bot.onText(/\/editable/, function onEditableText(msg) {
-  const opts = {
-    reply_markup: {
-      inline_keyboard: [
-        [
-          {
-            text: 'Edit Text',
-            // we shall check for this value when we listen
-            // for "callback_query"
-            callback_data: 'edit'
-          }
-        ]
-      ]
+// Matches /any word other than world and start or /start
+bot.onText(/^(?!.*(world|\/start|start)).*$/gim, async function onOther(msg, match) {
+  bot.sendMessage(msg.chat.id, "Your Request is processing...");
+  // console.log(match);
+  
+  var text = match[0] ;
+  var exist = checkCountry(text);
+  if(exist !== 0){
+    try {
+      var countryCode;
+      var countryName 
+      if( exist == 1){
+        
+        countryCode = text;
+        countryName = countries[countryCode]
+        // console.log("capital");
+      }
+      else{        
+        countryCode = rev_country[text.toLowerCase()]
+        countryName = text
+      }      
+      var response = await axios.get(`https://thevirustracker.com/free-api?countryTotal=${countryCode}`)
+      // console.log(countryCode,response.data.countrydata);
+      var data = response.data.countrydata[0]
+      
+      bot.sendMessage(msg.chat.id, `CoronaVirus covid-19 data of ${toTitleCase(countryName)} - ${countryCode.toUpperCase()}: 
+Confirmed Case: <b>${data.total_cases}</b> 
+Recovered:  <b>${data.total_recovered}</b> 
+Death:  <b>${data.total_deaths}</b>
+Total new cases today:  <b>${data.total_new_cases_today}</b>
+Total new deaths today: <b>${data.total_new_deaths_today}</b>
+Total active cases :    <b>${data.total_active_cases}</b>
+Total serious cases :   <b>${data.total_serious_cases}</b>`, {parse_mode: "HTML"})
+    } catch (error) {
+      console.log(error);
+      bot.sendMessage(msg.chat.id,"Sorry internal server error , try to contact @chapimenge")
     }
-  };
-  bot.sendMessage(msg.from.id, 'Original Text', opts);
-});
+  }else{
+    bot.sendMessage(msg.chat.id,"Please send valid command to see exaple send me /start or start")
 
-
-// Handle callback queries
-bot.on('callback_query', function onCallbackQuery(callbackQuery) {
-  const action = callbackQuery.data;
-  const msg = callbackQuery.message;
-  const opts = {
-    chat_id: msg.chat.id,
-    message_id: msg.message_id,
-  };
-  let text;
-
-  if (action === 'edit') {
-    text = 'Edited Text';
   }
-
-  bot.editMessageText(text, opts);
 });
+
+function checkCountry(name){
+  // console.log("Entered");
+  name  = name.toUpperCase();
+  for(var i in countries){
+    if ( i === name){
+      return 1;
+    }
+  }
+  name = name.toLowerCase();
+  for(var i in countries){
+    if ( countries[i] === name){
+      return 11;
+    }
+  }
+  
+  return 0;
+}
